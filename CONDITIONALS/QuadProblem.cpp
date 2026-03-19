ae7f25c2daf81a1df297fbafd79e26b46e7cb680
Pranav Arya <nowitspranav18@gmail.com>
2026-03-19 14:59:38 +0530

    Add conditional problems and update calculator

---
+++ CONDITIONALS/QuadProblem.cpp
@@ -0,0 +1,24 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x,y;
+//     cout<<"enter x : ";
+//     cin>>x;
+//     cout<<"enter y : ";
+//     cin>>y;
+//     if(x==0 && y==0){
+//         cout<<"at origin";
+//     } else if (y==0){
+//         cout<<"at x-axis";
+//     }else if (x==0){
+//         cout<<"at y-axis";
+//     }else if (x>0&&y>0){
+//         cout<<"first quad";
+//     }else if (x<0&&y>0){
+//         cout<<"second quad ";
+//     }else if (x<0&&y<0){
+//         cout<<"third quad";
+//     }else{
+//         cout<<"forth quad";
+//     }
+// }
---
+++ CONDITIONALS/sidesoftriangle.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     cout<<"enter a value of a :";
+//     cin>>a;
+//     cout<<"enter a value of b : ";
+//     cin>>b;
+//     cout<<"enter a value of c : ";
+//     cin>>c;
+//     if((a+b)>c && (b+c)>a && (c+a)>b){
+//         cout<<"yes , valid triangle ";
+//     }else {
+//         cout<<"invalid";
+//     }
+
+// } 
