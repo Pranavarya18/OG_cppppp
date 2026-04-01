79ff55e7a272d39c96c70762067bf4b53b879910
Pranav Arya <nowitspranav18@gmail.com>
2026-04-01 13:06:15 +0530

    Added GP and Table programs

---
+++ LOOPS/19table.cpp
@@ -0,0 +1,19 @@
+  // we are printing in two ways 
+//   #include<iostream>
+//   using namespace std;
+//   int main(){
+//     //172 times loops happening 
+//     for (int i=19;i<=190;i++){
+//         if(i%19==0) cout<<i<<" ";
+//     }
+//   }
+
+//   //now more optimize 
+//   #include <iostream>
+//   using namespace std;
+//   int main(){
+//     // 19 times loop is running
+//     for(int i=19;i<=190;i+=19){
+//     cout<<i<<" ";
+//     }
+//   }
---
+++ LOOPS/evennumber.cpp
@@ -0,0 +1,16 @@
+// // #include<iostream>
+// // using namespace std;
+// // int main(){
+// //     int i;
+// //     for (i=2;i<=100;i++){
+// //         if(i%2==0) cout<<i<<endl;
+// //     }
+// // }
+
+// #include<iostream>
+// using namespace std;
+// int main (){
+//     for(int i=1;i<=100;i++){
+//         if(i%2!=00) cout<<i<<endl;
+//     }
+// }
