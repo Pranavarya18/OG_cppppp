d282d49b0edde4184c521f2895f3669393e63c51
Pranav Arya <nowitspranav18@gmail.com>
2026-04-04 09:03:07 +0530

    first commit

---
+++ BREAK/composite.cpp
@@ -0,0 +1,27 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cout<<"Enter n : ";
+//     cin>>n;
+
+//     bool isComposite = false;
+
+//     for(int i=2; i<=n/2; i++){
+//         if(n % i == 0){
+//             isComposite = true;
+//             break;
+//         }
+//     }
+
+//     if(n <= 1){
+//         cout<<"Neither Prime nor Composite";
+//     }
+//     else if(isComposite){
+//         cout<<"Composite";
+//     }
+//     else{
+//         cout<<"Prime";
+//     }
+// }
--- LOOPS/GP.cpp
+++ LOOPS/GP.cpp
@@ -1,13 +1,27 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n ;
-    cout<<"enter n :";
-    cin>>n;
-    //1 2 4 8 16...
-    int a =1;
-    for(int i=1;i<=n;i++){
-        cout<<a<<" ";
-        a = a*2;
-    }
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n ;
+//     cout<<"enter n :";
+//     cin>>n;
+//     //1 2 4 8 16...
+//     int a =1;
+//     for(int i=1;i<=n;i++){
+//         cout<<a<<" ";
+//         a = a*2;
+//     }
+// }
+
+// //3,12,48...upto n term 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter n : ";
+//     cin>>n;
+//     int a=3;
+//     for (int i=1;i<=n;i++){
+//         cout<<a<<" ";
+//         a*=4;
+//     }
+// }
