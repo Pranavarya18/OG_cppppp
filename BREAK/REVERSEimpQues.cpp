4e741826a5020e4efc1dc2794ef5b257080d9e6d
Pranav Arya <nowitspranav18@gmail.com>
2026-04-10 00:54:48 +0530

    added patterns code

--- BREAK/REVERSEimpQues.cpp
+++ BREAK/REVERSEimpQues.cpp
@@ -1,16 +1,18 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int ld;
-    int r=0;
-    int n;
-    cout<<"enter n : ";
-    cin>>n;
-    while(n!=0){
-        ld=n%10;
-        r*=10;
-        r+=ld;
-        n=n/10;
-    }
-    cout<<r;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int ld;
+//     int r=0;
+//     int n;
+//     cout<<"enter n : ";
+//     cin>>n;
+//     while(n!=0){
+//         ld=n%10;
+//         r*=10;
+//         r+=ld;
+//         n=n/10;
+//     }
+//     cout<<r;
+// }
+
+// print the reverse of given number 
---
+++ PATTERN_PRINTING/numberpattern.cpp
@@ -0,0 +1,27 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter numbers: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=1;j++){
+//             cout<<"1234";
+//         }
+//         cout<<endl;
+//     }
+// }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter numbers: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+
+// }
